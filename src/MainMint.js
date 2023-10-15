764032fbaa4c2c6401992585afea4d30c329da5b
tungeekx <almatx78@gmail.com>
2023-10-15 17:09:15 +0600

    rr

--- src/MainMint.js
+++ src/MainMint.js
@@ -6,8 +6,11 @@ const skymanAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const MainMint = ({accounts, setAccounts}) =>{
     const isConnected = Boolean(accounts[0])
+    const [uri, setUri] = useState('')
 
     async function handleMint(){
+
+    
         if (window.ethereum){
             const provider = new ethers.BrowserProvider(window.ethereum)
             const signer = provider.getSigner()
@@ -18,7 +21,7 @@ const MainMint = ({accounts, setAccounts}) =>{
             )
 
             try {
-                const response = await contract.payToMint("")
+                const response = await contract.payToMint(accounts[0], {uri})
                 console.log("response: ", response)
                 
             } catch (error) {
@@ -32,10 +35,11 @@ const MainMint = ({accounts, setAccounts}) =>{
             <h1>Skyman NFT</h1>
             <p>It's 2023. Almat Alizhan and Aiym</p>
             {isConnected ? (
-                <div>
-                    <div>
-                        <input type="text" placeholder="URI"/>
-                    </div>
+                <div className="create">
+                    <form>
+                        <label>URI: </label>
+                        <input type="text" required value={uri} onChange={(e)=> setUri(e.target.value)}/>
+                    </form> 
                     <button onClick={handleMint}>Mint</button>
                 </div>
             ):(
